a72d40b6259bd15a8db089cac610515fd980ad21
Ben Lardie <benlardie@gmail.com>
2019-03-28 15:57:24 -0400

    finished section 1

--- first_ajax/static/js/main.js
+++ first_ajax/static/js/main.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
-  
+    buttonOne = document.querySelector('.button-one');
+    console.log(buttonOne)
+    buttonOne.addEventListener('click', function() {
+        axios.get('http://intro-ajax-api.herokuapp.com/')
+    });
+    buttonTwo = document.querySelector('.button-two');
+    buttonTwo.addEventListener('click', function() {
+        axios.get('http://intro-ajax-api.herokuapp.com/ping')
+        .then(function (response) {
+        console.log('response', response);
+        
+        });
+    });
+
 });
+    
