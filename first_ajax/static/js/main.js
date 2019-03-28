233e8d6822a1c81f886a6f474900cce70cb62f15
Ben Lardie <benlardie@gmail.com>
2019-03-28 17:07:46 -0400

    count works

--- first_ajax/static/js/main.js
+++ first_ajax/static/js/main.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     buttonTwo = document.querySelector('.button-two');
     buttonTwo.addEventListener('click', function() {
         section = document.querySelector('section');
-        axios.get('http://intro-ajax-api.herokuapp.com/pong')
+        axios.get('http://intro-ajax-api.herokuapp.com/ping')
         .then(function (response) {
         console.log('response', response);
         section.innerHTML = response.data;
@@ -19,6 +19,16 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(function() {
             console.log('this is then');
         })
+    });
+    const buttonThree = document.querySelector('.button-three');
+    const sectionSeven = document.querySelector('.section-7');
+    buttonThree.addEventListener('click', function() {
+        axios.get('http://intro-ajax-api.herokuapp.com/count')
+        .then(function (response) {
+            console.log(response);
+            sectionSeven.innerHTML = response.data;
+
+        });
     });
     
 
